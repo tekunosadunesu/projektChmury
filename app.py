288d299c10320456be1a4d862db5c4d5f2133f39
Dariusz <133689768+tekunosadunesu@users.noreply.github.com>
2025-06-04 11:59:15 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -13,13 +13,26 @@ import urllib.parse
 import psycopg2
 
 def get_connection_uri():
+
+    # Read URI parameters from the environment
     dbhost = os.environ['DBHOST']
     dbname = os.environ['DBNAME']
-    dbuser = os.environ['DBUSER']
-    dbpassword = os.environ['DBPASSWORD']
+    dbuser = urllib.parse.quote(os.environ['DBUSER'])
     sslmode = os.environ['SSLMODE']
 
-    db_uri = f"postgresql://{dbuser}:{dbpassword}@{dbhost}/{dbname}?sslmode={sslmode}"
+
+    # Use passwordless authentication via DefaultAzureCredential.
+    # IMPORTANT! This code is for demonstration purposes only. DefaultAzureCredential() is invoked on every call.
+    # In practice, it's better to persist the credential across calls and reuse it so you can take advantage of token
+    # caching and minimize round trips to the identity provider. To learn more, see:
+    # https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/identity/azure-identity/TOKEN_CACHING.md
+    credential = DefaultAzureCredential()
+
+    # Call get_token() to get a token from Microsft Entra ID and add it as the password in the URI.
+    # Note the requested scope parameter in the call to get_token, "https://ossrdbms-aad.database.windows.net/.default".
+    password = credential.get_token("https://ossrdbms-aad.database.windows.net/.default").token
+
+    db_uri = f"postgresql://{dbuser}:{password}@{dbhost}/{dbname}?sslmode={sslmode}"
     return db_uri
 
 def get_blob_service_client():
